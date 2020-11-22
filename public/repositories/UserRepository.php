113dafa6a725b04c90e1654d0c0d17cd2375a01b
Usman Mahmood <usman.mahmood@live.ca>
2020-11-21 21:22:47 -0500

    create user repository

---
+++ public/repositories/UserRepository.php
@@ -0,0 +1,25 @@
+<?php 
+
+require_once(__DIR__ . '/../data/Database.php');
+require_once(__DIR__ . '/../entities/User.php');
+
+class UserRepository
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getUserByUsernameAndPassword(string $username, string $password)
+    {
+        $sql = "SELECT * FROM Users where email = ? and password = ?";
+        $params = [$username, $password];
+        $result = $this->db->execute($sql, $params);
+        
+        return $result->fetchAll(PDO::FETCH_CLASS, 'User');
+    }
+}
+
+?>
