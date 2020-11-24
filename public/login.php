2305df395804503b50618fe9dd423a101684f19c
Usman Mahmood <usman.mahmood@live.ca>
2020-11-24 18:18:51 -0500

    added text/css to style tag

--- public/login.php
+++ public/login.php
@@ -33,7 +33,8 @@ if ($_POST)
     <title>Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
+    
+    <style type="text/css">
         .login-form {
             width: 340px;
             margin: 50px auto;
