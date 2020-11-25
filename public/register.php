f8510fcb78f2f58b7114eb5cd75fddb3486b28d9
Daniel <danielgrewal@gmail.com>
2020-11-25 00:34:35 -0500

    moved css to external file, updated register form

--- public/register.php
+++ public/register.php
@@ -1,29 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registration</title>
-	<link rel="stylesheet" href="assets/css/styles.css">
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
-	<h1>Registration Form</h1>
-	<!-- add a link to the log in page to the <p> below -->
-	<p>You must register before booking your appointment. If you have already registered, please click here to login.</p>
-	<!-- this is the box where the input form goes...-->
-	<div class="formbox">
-		<form action="validate.php" method="POST">
-			<fieldset>
-				<!-- we can add radio buttons for if patient is male/female, or if a senior or a child etc.-->
-				First Name: <input type="text" name="firstname" autocomplete="off"><br>
-				Last Name: <input type="text" name="lastname" autocomplete="off"><br>
-				<!-- use input type="email" once HTML5 check is done, ask Usman-->
-			    Email: <input type="text" name="email" autocomplete="off"><br>
-			    Password: <input type="password" name="password"><br>
-			    Confirm Password: <input type="password" name="passwordconfirm"><br><br>
-			    <input type="submit" value="Register">
-			</fieldset>
+	<div class="login-form">
+		<form id="login-form" action="validate.php" method="POST">
+			<h2 class="text-center">Registration Form</h2>
+			<p>You must register before booking an appointment. If you have already registered, <a href="login.php">click here</a> to login.</p>
+			<div class="form-group">
+				<label for="firstname">First Name</label>
+				<input type="text" class="form-control" name="firstname" autocomplete="off" required>
+			</div>
+			<div class="form-group">
+				<label for="lastname">Last Name</label>
+				<input type="text" class="form-control" name="lastname" autocomplete="off" required>
+			</div>
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input type="email" class="form-control" name="email" autocomplete="off" required>
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+			    <input type="password" class="form-control" name="password" required>
+			</div>
+			<div class="form-group">
+				<label for="passwordconfirm">Confirm Password</label>
+			    <input type="password" class="form-control" name="passwordconfirm" required>
+			</div>    	
+			<input type="submit" class="btn btn-primary btn-block" value="Register now">
 	    </form>
 	</div>
-	
-
 </body>
 </html>
