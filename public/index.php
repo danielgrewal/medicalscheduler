ff9799a5f59f4fbded05878b9bcbfe77f4c9d847
Daniel <danielgrewal@gmail.com>
2020-11-26 01:05:16 -0500

    added more to landing and css

--- public/index.php
+++ public/index.php
@@ -19,11 +19,54 @@
 	</div>
 	<header class="jumbotron">
     <div class="container-fluid text-center"> 
-       <h1 class="display-3"><strong>Your Health and Well-Being Matters!</strong></h1>
+       <h1 class="display-3"><strong>Your Health and Well-Being Matters.</strong></h1>
        <p class="lead pb-4"><strong>We are committed to providing the best health care for you and your family.</strong></p>
-       <p><a href="#" class="btn btn-primary btn-lg" role="button">Learn More</a></p>
     </div>
 	</header>
+<section id="services" class="container">
+   <h2 class="display-4 text-center mt-5 mb-3">Our Services</h2>
+   
+   <div class="row text-center">
+      <div class="col-md-4 mb-4">
+         <div class="card h-100">
+            <img class="card-img-top" src="" alt="DOCTOR PHOTO">
+            <div class="card-body">
+               <h4 class="card-title">Quality Care</h4>
+               <p class="card-text">we have the best doctors blah blah blah</p>
+            </div>
+            <div class="card-footer py-4">
+               <a href="#" class="btn btn-primary btn-block">See Doctors &raquo;</a>
+            </div>
+         </div>
+      </div>
+      
+      <div class="col-md-4 mb-4">
+         <div class="card h-100">
+            <img class="card-img-top" src="" alt="DOCTOR PHOTO">
+            <div class="card-body">
+               <h4 class="card-title">Easy Booking</h4>
+                  <p class="card-text">blah blah blah book online blah blah</p>
+            </div>
+            <div class="card-footer py-4">
+               <a href="#" class="btn btn-primary btn-block">See Schedule &raquo;</a>
+            </div>
+         </div>
+      </div>
+      
+      <div class="col-md-4 mb-4">
+         <div class="card h-100">
+            <img class="card-img-top" src="" alt="DOCTOR PHOTO">
+            <div class="card-body">
+               <h4 class="card-title">WHAT ELSE CAN WE PUT HERE?</h4>
+               <p class="card-text">maybe put testimonials or something here???</p>
+            </div>
+            <div class="card-footer py-4">
+               <a href="#" class="btn btn-primary btn-block">See WHAT? &raquo;</a>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
 	
 
 </body>
