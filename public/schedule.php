96053af5b6e81cf71345b5412211d6f3ab193cde
Usman Mahmood <usman.mahmood@live.ca>
2020-11-27 04:39:46 -0500

    reformatted schedule.php

--- public/schedule.php
+++ public/schedule.php
@@ -7,7 +7,6 @@ $user = AuthenticationService::authenticate();
 
 $appointmentService = new AppointmentService();
 $dates = $appointmentService->getDaysInWeekByDate(date('Y-m-d'));
-var_dump($dates);
 
 ?>
 
@@ -22,32 +21,30 @@ var_dump($dates);
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
-	<div class="topnav" id="myTopnav">
-  <a href="index.php">Home</a>
-  <a href="login.php">Sign in</a>
-  <a href="register.php">Register</a>
-  <a href="profiles.php">Our Doctors</a>
-  <a href="schedule.php" class="active">View Appointments</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
-</div>
+    <div class="topnav" id="myTopnav">
+        <a href="index.php">Home</a>
+        <a href="login.php">Sign in</a>
+        <a href="register.php">Register</a>
+        <a href="profiles.php">Our Doctors</a>
+        <a href="schedule.php" class="active">View Appointments</a>
+        <a href="#" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
 	<h1>HERE IS WHERE THE SCHEDULE WILL BE ETC ETC</h1>
 
-	    <div class="footer">
+    <div class="footer">
         <p class="footer_text">Copyright © SOFE2800 Final Project Group 3 - Fall 2020 @ Ontario Tech University</p>
     </div>
-    
+    <script type="text/javascript">
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                } else {
+                    x.className = "topnav";
+                }
+            }
+    </script>
 </body>
-
-<script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-</script>
 </html>
